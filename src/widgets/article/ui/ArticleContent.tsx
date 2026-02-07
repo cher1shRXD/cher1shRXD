26bc32db5bd0e28ada6aa34470e9afe701524a5c
김태우 <taewoo080401@gmail.com>
2026-02-07 22:45:17 +0900

    fix: quote block ui

--- src/widgets/article/ui/ArticleContent.tsx
+++ src/widgets/article/ui/ArticleContent.tsx
@@ -305,9 +305,9 @@ const BlockRenderer = ({ block }: { block: NotionBlockWithChildren }) => {
     case "quote": {
       return (
         <blockquote
-          className={`p-8 bg-surface rounded-lg md:rounded-xl my-4 ml-8 text-text/70 ${colorMap[block.quote.color] || ""} wrap-break-word relative`}>
-          <span className="text-2xl md:text-4xl text-primary h-5 absolute top-2 left-2">&ldquo;</span>
-          <span className="text-2xl md:text-4xl text-primary h-5 absolute bottom-2 right-2">&rdquo;</span>
+          className={`p-6.5 bg-surface rounded-lg md:rounded-xl my-4 ml-8 text-text/70 ${colorMap[block.quote.color] || ""} wrap-break-word relative`}>
+          <span className="text-2xl md:text-4xl text-primary h-5 absolute top-2.5 left-2.5">&ldquo;</span>
+          <span className="text-2xl md:text-4xl text-primary h-5 absolute bottom-2.5 right-2.5">&rdquo;</span>
           <RichText richText={block.quote.rich_text} />
           {block.children && block.children.length > 0 && (
             <div className="mt-2">
