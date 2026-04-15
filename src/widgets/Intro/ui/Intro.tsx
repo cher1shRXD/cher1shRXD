20d04da68f1f6f0d514c51e6a7c6718c56ec58c9
김태우 <taewoo080401@gmail.com>
2026-04-15 16:56:03 +0900

    fix: delete space

--- src/widgets/Intro/ui/Intro.tsx
+++ src/widgets/Intro/ui/Intro.tsx
@@ -4,7 +4,7 @@ import { useTyping } from "../hooks/useTyping";
 import { useInView } from "react-intersection-observer";
 
 const Intro = () => {
-  const text = "기술보다\n문제를 먼저 바라보는 개발자,\n 김태우입니다.";
+  const text = "기술보다\n문제를 먼저 바라보는 개발자,\n김태우입니다.";
   const { ref, inView } = useInView({
     threshold: 1,
   });
